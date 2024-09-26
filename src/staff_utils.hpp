09a963197c345dce2bfd8a2195ff3257b72564ad
ogawa-momo <t122018@m.chukyo-u.ac.jp>
2024-09-26 15:44:17 +0900

    fix: directory location

--- src/staff_utils.hpp
+++
@@ -1,9 +0,0 @@
-//編集不可
-
-#ifndef STAFF_UTILS_H_
-#define STAFF_UTILS_H_
-
-void bounding_box(Point point, int img_id, int t_H, int t_W);
-double euclidean_distance(double x1, double y1, double x2, double y2);
-
-#endif
